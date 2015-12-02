6d6ad4cb7eb32c9a05dbcbab67ca97437db48103
MrD <mrdmgdd@gmail.com>
2015-12-02 20:32:28 +0700

    ! Delete old file

--- Sources/BeHocVan/install_device.bat
+++
@@ -1,5 +0,0 @@
-
-			@echo off
-			cls
-			C:\AndroidSDK\platform-tools\adb -d install -r bin/BeHocVan.apk
-		
--- Sources/BeHocVan/install_emu.bat
+++
@@ -1,5 +0,0 @@
-
-			@echo off
-			cls
-			C:\AndroidSDK\platform-tools\adb install -r bin/BeHocVan.apk
-		
